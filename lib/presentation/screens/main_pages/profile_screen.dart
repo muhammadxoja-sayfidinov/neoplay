f5e08e4c46fddd4a116a8b6900f403826fef6600
musaymer <muhammadxojasayfidinov986@gamail.com>
2024-08-25 18:20:00 +0500

    test

--- lib/presentation/screens/main_pages/profile_screen.dart
+++ lib/presentation/screens/main_pages/profile_screen.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/colors.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('4',style: TextStyle(color: red),),);
+    return const Center(
+      child: Text(
+        '4',
+        style: TextStyle(color: red),
+      ),
+    );
   }
 }

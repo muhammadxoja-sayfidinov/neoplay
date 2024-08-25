9f3d08a36f412805516ca5a346a959257b48bd29
abduqahhor-001 <abduqahhora364@gmail.com>
2024-08-25 18:49:52 +0500

    add search and new color
    Signed-off-by: abduqahhor-001 <abduqahhora364@gmail.com>

---
+++ lib/presentation/screens/main_pages/search_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:neoplay/core/constants/colors.dart';
+import 'package:neoplay/core/constants/style.dart';
+class SearchScreen extends StatelessWidget {
+  final TextEditingController _searchController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.black,
+        padding: EdgeInsets.symmetric(vertical:72.w),
+        child: Column(
+          children: [
+              Container(
+                    decoration: BoxDecoration(
+                      color: grey,
+                      borderRadius: BorderRadius.only(bottomLeft:Radius.circular(60.r),bottomRight:Radius.circular(60.r))
+                    ),
+                child: Center(
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+
+                      icon: Icon(Icons.search , color: gilosNeutral,),
+                      hintText: "Film, serial, multfilm izlang",
+                      hintStyle: CustomTextStyle.style400.copyWith(fontSize: 24.sp , color: gilosNeutral),
+
+                    ),
+                  ),
+                )
+              ),
+
+            32.h.horizontalSpace,
+
+            Container(
+              decoration: BoxDecoration(
+
+              ),
+            )
+          ],
+        ),
+    );
+  }
+}
+
